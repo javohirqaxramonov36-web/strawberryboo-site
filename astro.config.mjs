e78b6eceb0d637db4251b92fee8011a77940c626
Javohir Kakhramonov <javohir@users.noreply.github.com>
2026-08-15 10:17:20 +0500

    Strawberryboo sayt: 3 ta kurs + deploy tayyor

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://javohirqaxramonov36-web.github.io',
+  base: '/strawberryboo-site',
+  // 8GB RAM uchun minimal config
+});
